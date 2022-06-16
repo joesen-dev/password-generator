142d7f45e8bca3931b7d4fbb880500ce9296438c
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-06-16 10:30:04 -0400

    password generating as intended

--- Develop/script.js
+++ Develop/script.js
@@ -43,41 +43,64 @@ var charTypeArray = [
    
 ];
 // PASSWORD CHOICES STORAGE
-var passowrdCriteriaArray = []
-    for (i = 0; i < passowrdCriteriaArray.length; i++) {
-        function logChoices() {
-            var logChoices = comfirm(passowrdCriteriaArray[i]);
+// var passowrdCriteriaArray = [passCrData1, passCrData2, passCrData3, passCrData4]
+    // for (i = 0; i < passowrdCriteriaArray.length; i++) {
+    //     function logChoices() {
+    //         var logChoices = comfirm(passowrdCriteriaArray[i]);
 
-            if (passowrdCriteriaArray[i] = {name: "lowerCase", value: Array(26)}) {                
-                var passCrData1 = passowrdCriteriaArray[0];
-                passCrData1.push(passowrdCriteriaArray[0]); 
+    //         if (passowrdCriteriaArray[0] = {name: "lowerCase", value: Array(26)}) {                
+    //             var passCrData1 = passowrdCriteriaArray[0];
+    //             passCrData1.push(passowrdCriteriaArray[0]); 
 
-            } else if (passowrdCriteriaArray[i++] = {name: "upperCase", value: Array(26)}) {
-                var passCrData2 = passowrdCriteriaArray[1];
-                passCrData2.push(passowrdCriteriaArray[1]);
+    //         } if (passowrdCriteriaArray[1] = {name: "upperCase", value: Array(26)}) {
+    //             var passCrData2 = passowrdCriteriaArray[1];
+    //             passCrData2.push(passowrdCriteriaArray[1]);
 
-            } else if (passowrdCriteriaArray[i++] = {name: "numeric", value: Array(10)}) {
-                var passCrData3 = passowrdCriteriaArray[2];
-                passCrData3.push(passowrdCriteriaArray[2]);
+    //         } if (passowrdCriteriaArray[2] = {name: "numeric", value: Array(10)}) {
+    //             var passCrData3 = passowrdCriteriaArray[2];
+    //             passCrData3.push(passowrdCriteriaArray[2]);
 
-            } else if (passowrdCriteriaArray[i++] = {name: "specialChar", value: Array(32)}) {
-                var passCrData4 = passowrdCriteriaArray[3];
-                passCrData4.push(passowrdCriteriaArray[3]);
+    //         } if (passowrdCriteriaArray[3] = {name: "specialChar", value: Array(32)}) {
+    //             var passCrData4 = passowrdCriteriaArray[3];
+    //             passCrData4.push(passowrdCriteriaArray[3]);
 
-            } else {
-                (passowrdCriteriaArray[i] = passowrdCriteriaArray[i])
-                var passCrData1 = passowrdCriteriaArray[0];
-                var passCrData2 = passowrdCriteriaArray[1];
-                var passCrData3 = passowrdCriteriaArray[2];
-                var passCrData4 = passowrdCriteriaArray[3];                
-            } 
-            console.log(logChoices)
-            return logChoices
-        }
+    //         } else if (passowrdCriteriaArray[i] = null) {
+    //             characterType()
+    //         } {
+    //             (passowrdCriteriaArray[i])
+    //             var passCrData1 = passowrdCriteriaArray[0];
+    //             var passCrData2 = passowrdCriteriaArray[1];
+    //             var passCrData3 = passowrdCriteriaArray[2];
+    //             var passCrData4 = passowrdCriteriaArray[3];                
+    //         } 
+    //         console.log(logChoices)
+    //         return logChoices
+    //     }
 
-    }
+    // }
 
-var characterAmount = passowrdCriteriaArray
+// ------------------
+// var passwordLength = function() {
+//     // debugger;
+//     charLength = "";
+//     while (charLength === "" || charLength === null) {
+//         charLength = prompt("How many characters will your password be? Enter a number between 8 and 128.");
+//     }
+//     // User must enter an number btween 8-128 or they're looped back to request how many characters are in the password
+//     if (charLength < 8 || charLength > 128 || isNaN(charLength)) {
+//         alert("Please choose a number between 8 and 128");
+//         return passwordLength();
+//     } else { 
+//         alert("Your password will have " + charLength + " characters");
+//     }
+//     console.log("Your password will have " + charLength + " characters");
+//     // here we push() the user's entry into our passwordCriteria Array for recall later
+//     characterAmountArray.push(charLength);
+//     console.log(characterAmountArray);
+//     return charLength;
+// }
+// ---------------------------
+// var characterAmount = passowrdCriteriaArray
 
 var characterAmountArray = []
 // password length data
@@ -85,15 +108,10 @@ var passLengthData = characterAmountArray[0]
 
 
 // password criteria data
-var passCrData1 = []
-var passCrData2 = []
-var passCrData3 = []
-var passCrData4 = []
-
-var includeLowerCase = passCrData1
-var includeUpperCase = passCrData2
-var includeNumbers = passCrData3
-var includeSymbols = passCrData4
+// var passCrData1 = []
+// var passCrData2 = []
+// var passCrData3 = []
+// var passCrData4 = []
 
 
 // Create a charater type object then convert it into an Array: START
@@ -131,26 +149,26 @@ var passwordLength = function() {
     return charLength;
 }
 
-// CHARACTER TYPE PROMPT
-var characterType = function() {
-  for (var i = 0; i < charTypePrompt.length; i++) {
-        function confirmAction() {
-        var confirmAction = confirm(charTypePrompt[i]);
+// // CHARACTER TYPE PROMPT
+// var characterType = function() {
+//   for (var i = 0; i < charTypePrompt.length; i++) {
+//         function confirmAction() {
+//         var confirmAction = confirm(charTypePrompt[i]);
         
-        if (confirmAction) {
-            console.log(charTypeArray[i])
-            passowrdCriteriaArray.push(charTypeArray[i]);
-            console.log(passowrdCriteriaArray );
-            // VALIDATE USER INPUT
-            alert("Your password will include " + userAlertArray[i] + " characters") 
-        } else {
-            return[];
-        }  
-        } confirmAction()
+//         if (confirmAction) {
+//             console.log(charTypeArray[i])
+//             passowrdCriteriaArray.push(charTypeArray[i]);
+//             console.log(passowrdCriteriaArray);
+//             // VALIDATE USER INPUT
+//             alert("Your password will include " + userAlertArray[i] + " characters") 
+//         } else {
+//             return[];
+//         }  
+//         } confirmAction()
         
-    }
-    // debugger;
-}
+//     }
+//     // debugger;
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -166,17 +184,46 @@ function writePassword() {
     // debugger;
      
     // ask for character length  
-    characterType();
+    // characterType();
+    // set variabls pasCrData1 - 4 to be the return of the 'confirm' for diiffernt charactr sets
+    passCrData1 = confirm("Would you like to include lowercase characters?")
+        if (passCrData1) {        
+        alert("Your password will include " + userAlertArray[0] + " characters")
+        passCrData1 = true
+        } else passCrData1 = false
+        console.log(passCrData1)
+    passCrData2 = confirm("Would you like to include uppercase characters?")
+        if (passCrData2) {        
+        alert("Your password will include " + userAlertArray[1] + " characters")
+        passCrData2 = true
+        } else passCrData2 = false
+        console.log(passCrData2)
+    passCrData3 = confirm("Would you like to include numeric characters?")
+        if (passCrData3) {        
+        alert("Your password will include " + userAlertArray[2] + " characters")
+        passCrData3 = true
+        } else passCrData3 = false
+        console.log(passCrData1)
+    passCrData4 = confirm("Would you like to include special characters?")
+        if (passCrData4) {        
+        alert("Your password will include " + userAlertArray[3] + " characters")
+        passCrData4 = true
+        } else passCrData4 = false
+        console.log(passCrData4)
+    
+    var passowrdCriteriaArray = [passCrData1, passCrData2, passCrData3, passCrData4]
+    // passCrData2 = false;
   
     createPassword(parseInt(characterAmountArray[0]), passCrData1, passCrData2, passCrData3, passCrData4) 
   
     function createPassword(passLengthData, passCrData1, passCrData2, passCrData3, passCrData4) {   
-        debugger;       
+        debugger;    
+    
         var charCodes = passowrdCriteriaArray; // ((passowrdCriteriaArray[i]))
-        if (passCrData1) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES) 
+        // if (passCrData1) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES) 
         if (passCrData2) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES) 
-        if (passCrData3) charCodes = charCodes.concat(SPECIAL_CHAR_CODES)
-        if (passCrData4) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+        if (passCrData4) charCodes = charCodes.concat(SPECIAL_CHAR_CODES)
+        if (passCrData3) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
         
         const password = []
         for (i = 0; i < passLengthData; i++) {
